3230f06df9ac618a7d3a53769de00b40a5956140
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-04-11 23:11:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //暗号化されたメッセージのリンクを生成します。
+function copyToClipboard (tagValue) {
+  if (navigator.clipboard) { // navigator.clipboardが使えるか判定する
+    return navigator.clipboard.writeText(tagValue).then(function () { // クリップボードへ書きむ
+      messageActive() //メッセージを表示する
+    })
+  } else {
+    tagText.select() // inputタグを選択する
+    document.execCommand('copy') // クリップボードにコピーする
+    messageActive() //メッセージを表示する
+  }
+}
 let msg = prompt("メッセージを入力しろぉおおおおおお");
 alert("コピーした。でも端末によってはできないかもね。。。");
 let nanka = "1998ky262.github.io/decode?d=";
 let value = nanka+decodeURI(msg);
-navigator.clipboard.writeText(value);
+copyToClipboard(value);
